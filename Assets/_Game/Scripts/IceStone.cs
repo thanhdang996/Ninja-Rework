39669d69e27158e46a41671763f117124c572778
thanhdang996 <blackholeyoshitsune@gmail.com>
2023-02-04 17:12:04 +0700

    -add băng rơi

---
+++ Assets/_Game/Scripts/IceStone.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceStone : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] private GameObject hitVFX;
+
+    private void Start()
+    {
+        OnInit();
+    }
+    private void OnInit()
+    {
+        rb.velocity = new(-2f, -speed);
+        CancelInvoke();
+        Invoke(nameof(OnDespawn), 3f);
+    }
+
+    private void OnDespawn()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Character>().OnHit(10f);
+            Instantiate(hitVFX, transform.position, transform.rotation);
+            OnDespawn();
+        }
+    }
+}
---
+++ Assets/_Game/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private IceStone iceStonePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnIceStone());
+    }
+
+    private IEnumerator SpawnIceStone()
+    {
+        while (true)
+        {
+            Vector3 pos = transform.position;
+            pos.x = Random.Range(-2f, 2f);
+            Instantiate(iceStonePrefab, pos, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+            pos.x = Random.Range(-2f, 2f);
+            Instantiate(iceStonePrefab, pos, Quaternion.identity);
+            yield return new WaitForSeconds(Random.Range(0.5f, 1f));
+        }
+    }
+}
